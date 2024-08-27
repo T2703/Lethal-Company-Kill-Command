d1d8cb3c073e190fb7102a8c6f5e9ee10c8668f3
Tristan <ricemaking69@gmail.com>
2024-08-27 18:34:31 -0500

    FINALLY FIXED THE ISSUES I HOPE

--- Patches/PlayerControllerBPatch.cs
+++ Patches/PlayerControllerBPatch.cs
@@ -7,17 +7,32 @@ namespace FirstMod.Patches
 {
 
     [HarmonyPatch(typeof(PlayerControllerB))]
-    internal class PlayerControllerBPatch
+    
+    internal class PlayerControllerBPatch 
     {
         static LcInputStuff lcInputStuff = LcInputStuff.Instance;
+        static System.Random randomDeath = new System.Random();
+
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
 
-        static void killButton(PlayerControllerB __instance)
+        static void killButton(PlayerControllerB __instance, bool ___isTypingChat, bool ___inTerminalMenu)
         {
-            if (lcInputStuff.DeathKey.triggered)
+            int[] randomDeathChoice = { 0, 1, 2 };
+            int randomDeathIndex = randomDeath.Next(randomDeathChoice.Length);
+            if (___isTypingChat || ___inTerminalMenu) 
+            {   
+                lcInputStuff.DeathKey.Disable();
+                return;
+            }
+            else
             {
-                __instance.KillPlayer(Vector3.zero);
+                lcInputStuff.DeathKey.Enable();
+                if ((__instance = GameNetworkManager.Instance.localPlayerController) && lcInputStuff.DeathKey.triggered) 
+                {
+                    __instance.KillPlayer(Vector3.zero, true, CauseOfDeath.Strangulation, deathAnimation: randomDeathChoice[randomDeathIndex]);
+                    
+                }
             }
         }
 
